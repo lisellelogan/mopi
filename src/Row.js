fb522bc2513b909d32bfce52418482b769d52ec1
lisellelogan <liselle.logan@yahoo.com>
2021-12-15 16:22:38 -0500

    display movie title through props an add useState to manage state

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Row() {
+function Row({ title }) {
+  const [movies, setMovies] = useState([]);
+
   return (
     <div>
-      {/* title */}
+      <h2>{title}</h2>
 
       {/* container -> posters */}
     </div>
