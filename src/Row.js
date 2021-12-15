59c248dfdeb90a0912d370736919a670400b0a18
lisellelogan <liselle.logan@yahoo.com>
2021-12-15 16:39:02 -0500

    add fetchUrl to props and console.log to make sure everything is working

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     // if [], run once when row loads, and dont run again
     async function fetchData() {
-      const request = await axios.get(fetchUrll);
+      const request = await axios.get(fetchUrl);
+      console.log(request);
     }
   }, []);
 
